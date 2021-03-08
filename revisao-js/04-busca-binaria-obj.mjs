0f0dd1e14e3fd301c6c19126657e8cf2332bcb5d
gabrielmijoler <gabrielmijoler@hotmail.com>
2021-03-08 11:13:58 +0000

    correto

--- revisao-js/04-busca-binaria-obj.mjs
+++ revisao-js/04-busca-binaria-obj.mjs
@@ -1,19 +1,18 @@
+let comps = 0   // Número de comparações
 
-
-let comps = 0
-
-function buscabinaria(vetor, valorbusca) {
+function buscaBinaria(vetor, fnComp) {
     comps = 0
     let ini = 0
     let fim = vetor.length - 1
     let meio
     while (fim >= ini) {
         meio = Math.floor((fim + ini) / 2) // Math.floor() arredonda para baixo
-        if (fncomp(vetor[meio]) === 0) {
+        //console.log({ini, fim, meio, valorBusca})
+        if (fnComp(vetor[meio]) === 0) {
             comps++
-            return meio // achou
+            return meio // Achou
         }
-        else if (fncomp(vetor[meio] < 0)) {
+        else if (fnComp(vetor[meio]) < 0) {
             comps += 2
             fim = meio - 1
         }
@@ -22,34 +21,55 @@ function buscabinaria(vetor, valorbusca) {
             ini = meio + 1
         }
     }
-    return -1// valor nao existe no vetor
+    return -1   // Valor de busca não existe no vetor
 }
-// retornos da arropw function para busca binaria
-// 0 igualdade
-// -1 o valor de busca eh MENOR  que o valor do objeto comparado
-// +1 o valor de busca eh MAIOR  que o valor do objeto comparado
 
-const comparanomebin = Obj => {
-    if (obj.first_name === 'FAUSTO') return 0
-    else if ('FAUSTO' < obj.first_name) return -1
+// Retornos da arrow function para busca binária
+//  0: igualdade
+// -1: o valor de busca é MENOR que o valor do objeto comparado
+//  1: o valor de busca é MAIOR que o valor do objeto comparado 
+const comparaNomeBin = obj => {
+    if(obj.first_name === 'FAUSTO') return 0
+    else if('FAUSTO' < obj.first_name) return -1
     else return 1
 }
 
-import { objnomes } from '../includes/vetor-nomes.mjs'
-console.time("Busca ORKUTILSON")
-console.log(buscabinaria(objnomes, obj => {
-    const valorbusca = 'ORKUTILSON'
-    if (objnomes.first_name === valorbusca) return 0
-    else if (valorbusca < obj.first_name) return -1
+import { objNomes } from './includes/vetor-obj-nomes.mjs'
+
+console.time('Buscando ZILMAR')
+console.log(buscaBinaria(objNomes, obj => {
+    const valorBusca = 'ZILMAR'
+    if(obj.first_name === valorBusca) return 0
+    else if(valorBusca < obj.first_name) return -1
     else return 1
-}), { comps })
+}), {comps})
+console.timeEnd('Buscando ZILMAR')
 
-console.time("Busca ABRAAO")
-console.log(buscabinaria(nomes, "ABRAAO")
+console.time('Buscando KATIUSCIA')
+console.log(buscaBinaria(objNomes, obj => {
+    const valorBusca = 'KATIUSCIA'
+    if(obj.first_name === valorBusca) return 0
+    else if(valorBusca < obj.first_name) return -1
+    else return 1
+}), {comps})
+console.timeEnd('Buscando KATIUSCIA')
 
-console.time("Busca FAUSTO")
-console.log(buscabinaria(nomes, "FAUSTO")
+console.time('Buscando nome ABRAAO')
+let posEncontrado = buscaBinaria(objNomes, obj => {
+    const valorBusca = 'ABRAAO'
+    if(obj.first_name === valorBusca) return 0
+    else if(valorBusca < obj.first_name) return -1
+    else return 1
+})
+console.timeEnd('Buscando nome ABRAAO')
 
+console.log(objNomes[posEncontrado], {comps})
 
-console.time("Busca ZELIA")
-console.log(buscabinaria(nomes, "ZELIA")
+console.time('Buscando group_name MARIA')
+console.log(buscaBinaria(objNomes, obj => {
+    const valorBusca = 'MARIA'
+    if(objNomes.group_name === valorBusca) return 0
+    else if(valorBusca < obj.group_name) return -1
+    else return 1
+}), {comps})
+console.timeEnd('Buscando group_name MARIA')
