bd3cc44895ccb53f7b65e58cfdab546aa7ef5899
gabrielmijoler <gabrielmijoler@hotmail.com>
2021-08-31 15:44:56 +0000

    asd

--- revisao-js/07-selection-sort.mjs
+++ revisao-js/07-selection-sort.mjs
@@ -14,7 +14,8 @@ function selectionSort(vetor) {
 
     trocas = 0, pass = 0, comps = 0
     
-   
+    function encontrarMenor(inicio) {
+        let menor = inicio
         for(let j = inicio + 1; j < vetor.length; j++) {
             if(vetor[j] < vetor[menor]) menor = j
             comps++
@@ -37,7 +38,12 @@ function selectionSort(vetor) {
 
 //let nums = [7, 4, 9, 0, 6, 1, 8, 2, 5, 3]
 // Pior caso
-let nums = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-selectionSort(nums)
-console.log(nums)
-console.log({trocas, pass, comps})
+let nums = [9,0,1,2,3,4,5,6,7,8 ]
+//selectionSort(nums)
+//console.log(nums)
+// console.log({trocas, pass, comps})
+
+import { nomes } from '../includes/nomes-desor.mjs'
+console.time('comeco')
+selectionSort(nomes)
+console.timeEnd('fim')
